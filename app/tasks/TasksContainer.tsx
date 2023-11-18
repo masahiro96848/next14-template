e08c75a790e67838c01a5e49f1104ea155028348
masahiro96848 <feetwcpic9436@gmail.com>
2023-11-18 11:24:33 +0900

    ソースコードのリファクタリング

--- app/tasks/TasksContainer.tsx
+++ app/tasks/TasksContainer.tsx
@@ -7,20 +7,19 @@ import { TaskType } from '@/types/Task'
 export const TasksContainer = () => {
   const [tasks, setTasks] = useState<TaskType[] | undefined>(undefined)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetchTaskListApi()
-        if (response.code === 200 && response !== undefined) {
-          setTasks(response.data)
-        } else {
-          console.error('Error fetching task list')
-        }
-      } catch (error) {
-        console.error('Error fetching task list', error)
+  const fetchData = async () => {
+    try {
+      const response = await fetchTaskListApi()
+      if (response.code === 200 && response !== undefined) {
+        setTasks(response.data)
+      } else {
+        console.error('Error fetching task list')
       }
+    } catch (error) {
+      console.error('Error fetching task list', error)
     }
-
+  }
+  useEffect(() => {
     fetchData()
   }, [])
 
